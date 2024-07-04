32c9ec732152135a167c58d34c58b53b215106f2
blaze <blaze@DESKTOP-7R2GDLR>
2024-07-04 18:53:12 -0400

    add DbContext add ConnectionString to DB created 'Tareas' entity create initial 'Tareas' controller add migrations

---
+++ Controllers/TareasController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TareasApp.Entities;
+
+namespace TareasApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareasController : ControllerBase
+    {
+        public TareasController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTareas()
+        {
+            var tareas = new List<Tarea>
+            {
+                new Tarea {
+                    Id = 1,
+                    Title ="Mi primera tarea",
+                }
+            };
+
+            return Ok(tareas);
+        }
+    }
+}
